a8005fe131865861dc1aa02e86f8a0f6dfc3ead8
OxoGhost <arlenghost@proton.me>
2026-08-19 19:57:57 +0200

    fix: save ISO between make, instead of all at the end

--- make_release.sh
+++ make_release.sh
@@ -1,16 +1,22 @@
 VERISON=26.08-1
 SAVE_TO="../../SynologyDrive/Code_things/Others/Kira-Linux-Milestones/"
 
+mkdir -p "$SAVE_TO"/"$VERISON"
 
 echo "Building console ISO..."
 make clean && make console
+echo "Saving console ISO..."
+cp build/kira-console.iso "$SAVE_TO"/"$VERISON"/
 
 echo "Building desktop Sleex ISO..."
 make clean && make desktop DE=sleex
+echo "Saving desktop Sleex ISO..."
+cp build/kira-desktop-sleex.iso "$SAVE_TO"/"$VERISON"/
 
 echo "Building desktop SwayFX ISO..."
 make clean && make desktop DE=swayFX
+echo "Saving desktop SwayFX ISO..."
+cp build/kira-desktop-swayFX.iso "$SAVE_TO"/"$VERISON"/
 
-echo "Saving ISOs..."
-mkdir -p "$SAVE_TO"/"$VERISON"
-cp build/kira-*.iso "$SAVE_TO"/"$VERISON"/
+echo ""
+echo "Done !"

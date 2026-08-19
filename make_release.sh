ed44548ff7824cd92ddc6348e0f1e9adf312d462
OxoGhost <arlenghost@proton.me>
2026-08-19 22:23:10 +0200

    fix: added sudo to the make commands

--- make_release.sh
+++ make_release.sh
@@ -4,17 +4,17 @@ SAVE_TO="../../SynologyDrive/Code_things/Others/Kira-Linux-Milestones/"
 mkdir -p "$SAVE_TO"/"$VERISON"
 
 echo "Building console ISO..."
-make clean && make console
+sudo make clean && sudo make console
 echo "Saving console ISO..."
 cp build/kira-console.iso "$SAVE_TO"/"$VERISON"/
 
 echo "Building desktop Sleex ISO..."
-make clean && make desktop DE=sleex
+sudo make clean && sudo make desktop DE=sleex
 echo "Saving desktop Sleex ISO..."
 cp build/kira-desktop-sleex.iso "$SAVE_TO"/"$VERISON"/
 
 echo "Building desktop SwayFX ISO..."
-make clean && make desktop DE=swayFX
+sudo make clean && sudo make desktop DE=swayFX
 echo "Saving desktop SwayFX ISO..."
 cp build/kira-desktop-swayFX.iso "$SAVE_TO"/"$VERISON"/
 

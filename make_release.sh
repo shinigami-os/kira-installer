683397f911bbf3eb88a717276c922143a851e420
OxoGhost <arlenghost@proton.me>
2026-08-24 15:35:14 +0200

    feat: auto push-to-website

--- make_release.sh
+++ make_release.sh
@@ -1,22 +1,21 @@
-VERISON=26.08-1
-SAVE_TO="../../SynologyDrive/Code_things/Others/Kira-Linux-Milestones/"
+VERISON=26.08-3
 
-mkdir -p "$SAVE_TO"/"$VERISON"
+set -e
 
 echo "Building console ISO..."
 sudo make clean && sudo make console
-echo "Saving console ISO..."
-cp build/kira-console.iso "$SAVE_TO"/"$VERISON"/
+echo "=========================== Saving console ISO... ==========================="
+../kira-base/scripts/push-iso-release.sh $VERISON console
 
 echo "Building desktop Sleex ISO..."
 sudo make clean && sudo make desktop DE=sleex
-echo "Saving desktop Sleex ISO..."
-cp build/kira-desktop-sleex.iso "$SAVE_TO"/"$VERISON"/
+echo "=========================== Saving desktop Sleex ISO... ==========================="
+../kira-base/scripts/push-iso-release.sh $VERISON sleex
 
 echo "Building desktop SwayFX ISO..."
 sudo make clean && sudo make desktop DE=swayFX
-echo "Saving desktop SwayFX ISO..."
-cp build/kira-desktop-swayFX.iso "$SAVE_TO"/"$VERISON"/
+echo "=========================== Saving desktop SwayFX ISO... ==========================="
+../kira-base/scripts/push-iso-release.sh $VERISON swayfx
 
 echo ""
-echo "Done !"
+echo "Done ! "
